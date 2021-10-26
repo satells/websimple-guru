ae38040c6bc1bf40d6697dd971cbe62d01163be9
satells <msergiost@hotmail.com>
2021-10-26 11:31:49 -0300

    .

---
+++ websimple/src/test/java/com/simple/websimple/WebsimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.websimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
