c988869b93fdf3b030342851a6981b29c1fbb660
satells <msergiost@hotmail.com>
2021-10-26 14:31:03 -0300

    .

---
+++ websimple/src/main/java/com/simple/websimple/model/Author.java
@@ -0,0 +1,68 @@
+package com.simple.websimple.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String firstName;
+	private String lastName;
+	@ManyToMany
+	@JoinTable(name = "author_book")
+	private List<Book> books;
+
+	public Author() {
+
+	}
+
+	public Author(long id, String firstName, String lastName, List<Book> books) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.books = books;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+}
