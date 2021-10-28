3fd47fae8baa36fe13d17c6f6d57c3bd9125f586
satells <msergiost@hotmail.com>
2021-10-28 14:41:02 -0300

    .

--- websimple/src/main/java/com/simple/websimple/WebsimpleApplication.java
+++ websimple/src/main/java/com/simple/websimple/WebsimpleApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class WebsimpleApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
+
+		System.out.println("fdsafd");
 		SpringApplication.run(WebsimpleApplication.class, args);
 	}
 }
